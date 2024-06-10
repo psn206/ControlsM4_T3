57f02681740fccd770223f644b3039ab23e9ea9b
Sergei Prutsakov <psn206@mail.ru>
2024-06-10 15:15:22 +0300

    Добавлен класс MyCommands

---
+++ Controls/MyCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Controls
+{
+    internal class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            Exit = new RoutedCommand("Закрыть", typeof(MyCommands));
+        }
+
+    }
+}
