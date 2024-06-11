95b01ce6556a04e42b13322c261ed1eca868101e
Sergei Prutsakov <psn206@mail.ru>
2024-06-11 10:55:57 +0300

    Добавлен словарь ресурсов ComboxCource содержащий стили текста и размер шрифта

--- Controls/MainWindow.xaml.cs
+++ Controls/MainWindow.xaml.cs
@@ -29,13 +29,13 @@ namespace Controls
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = ((sender as ComboBox).SelectedItem as string);
             textBox.FontFamily = new FontFamily(fontName);
         }
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            double size = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            double size =Convert.ToDouble((sender as ComboBox).SelectedItem);
             textBox.FontSize = size;
         }
 
